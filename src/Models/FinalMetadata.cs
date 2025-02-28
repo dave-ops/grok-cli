ed9c50cfe6c9739f3d8eeac505877b70be0955e0
dave-ops <dave-ops@codeforge.cc>
2025-02-27 20:49:44 -0600

    added final meta model

---
+++ src/Models/FinalMetadata.cs
@@ -0,0 +1,10 @@
+namespace GrokCLI.Models
+{
+    public class FinalMetadata
+    {
+        public string[]? followUpSuggestions { get; set; }
+        public string[]? feedbackLabels { get; set; }
+        public object? toolsUsed { get; set; }
+        public string? disclaimer { get; set; }
+    }
+}
