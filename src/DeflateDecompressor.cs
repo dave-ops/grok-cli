5b095f2294460a09f0549f4449c13a3335debb85
dave-ops <dave-ops@codeforge.cc>
2025-02-28 00:11:52 -0600

    added deflate decompressor

---
+++ src/DeflateDecompressor.cs
@@ -0,0 +1,24 @@
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace GrokCLI.Decompressors
+{
+    public class DeflateDecompressor : IDecompressor
+    {
+        public async Task<byte[]> DecompressAsync(byte[] compressedData)
+        {
+            using (var memoryStream = new MemoryStream(compressedData))
+            using (var deflateStream = new DeflateStream(memoryStream, CompressionMode.Decompress))
+            using (var decompressedStream = new MemoryStream())
+            {
+                await deflateStream.CopyToAsync(decompressedStream);
+                return decompressedStream.ToArray();
+            }
+        }
+
+        public bool SupportsEncoding(string encoding)
+        {
+            return encoding?.Contains("deflate", StringComparison.OrdinalIgnoreCase) == true;
+        }
+    }
+}
