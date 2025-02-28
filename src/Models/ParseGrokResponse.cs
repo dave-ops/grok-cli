88734ac99b99579b1864b4708d4a39392c1f1899
dave-ops <dave-ops@codeforge.cc>
2025-02-27 20:51:44 -0600

    added model response model

--- src/Models/ParseGrokResponse.cs
+++ src/Models/ParseGrokResponse.cs
@@ -4,34 +4,6 @@ using System.Text;
 
 namespace GrokCLI.Models
 {
-
-
-    public class ModelResponse
-    {
-        public string? responseId { get; set; }
-        public string? message { get; set; }
-        public string? sender { get; set; }
-        public DateTime createTime { get; set; }
-        public string? parentResponseId { get; set; }
-        public bool manual { get; set; }
-        public bool partial { get; set; }
-        public bool shared { get; set; }
-        public string? query { get; set; }
-        public string? queryType { get; set; }
-        public object[]? webSearchResults { get; set; }
-        public string[]? xpostIds { get; set; }
-        public object[]? xposts { get; set; }
-        public string[]? generatedImageUrls { get; set; }
-        public object[]? imageAttachments { get; set; }
-        public object[]? fileAttachments { get; set; }
-        public object[]? cardAttachmentsJson { get; set; }
-        public string[]? fileUris { get; set; }
-        public object[]? fileAttachmentsMetadata { get; set; }
-        public bool isControl { get; set; }
-        public object[]? steps { get; set; }
-        public string[]? mediaTypes { get; set; }
-    }
-
     public class Result
     {
         public Conversation? conversation { get; set; }
